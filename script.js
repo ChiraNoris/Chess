e4b6aac1d6a8a302bd02aac8fdf7bf9d8cb6ad6b
Chira Noris Calin <norisnoris118@gmail.com>
2023-07-18 04:57:28 +0300

    #UPDATE

---
+++ script.js
@@ -0,0 +1,6 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+width = 8;
+
+const startPieces = [];
